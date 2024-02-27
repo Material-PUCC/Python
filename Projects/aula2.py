323429c77d454e1aede181bc66ca863fba0100ba
davMac606 <@worksender053@gmail.com>
2024-02-27 19:53:59 -0300

    Trying to save people from gore(aka onlinegdb)

--- Projects/aula2.py
+++ Projects/aula2.py
@@ -1,1 +1,6 @@
-print("Hello World!")
+print("Data detection program.")
+name=input("Enter your name: ")
+print("Welcome, ", name,". Is this your first time using this program?", sep="")
+answer=input("Yes or No: ")
+if answer=="No":
+    print("Welcome back, ", name, ".")
